c273ea8af3ce9f27c9c0b283711b17d6d0902e40
vygandaseidukis <vygandaseidukis@gmail.com>
2020-01-19 16:13:01 +0200

    added limit to fall speed
    fall speed limit
    fixed bug falling trough ground

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -8,21 +8,19 @@ public class PlayerMovement : MonoBehaviour, IPlayerMovement
     [SerializeField] private float _maxHorizontalSpeed;
     [SerializeField] private float _maxVerticalSpeed;
     [SerializeField] private Rigidbody2D _rigidBody;
+    [SerializeField] private float _maxFallSpeed;
     private Vector3 _velocity = Vector3.zero;
 
     private void Update()
     {
-        GroundCollisionCheck();
+        SpeedLimiter();
     }
-
 
     public void HorizontalMovement(float input)
     {
 
         // Move the character by finding the target velocity
         Vector3 targetVelocity = new Vector2(input * _maxHorizontalSpeed, _rigidBody.velocity.y);
-
-        Debug.Log(targetVelocity);
 
         // And then smoothing it out and applying it to the character
         _rigidBody.velocity = Vector3.SmoothDamp(_rigidBody.velocity, targetVelocity, ref _velocity, _horizontalAcceleration);
@@ -35,8 +33,9 @@ public class PlayerMovement : MonoBehaviour, IPlayerMovement
         _rigidBody.AddForce(new Vector2(0f, _maxVerticalSpeed));
     }
 
-    private void GroundCollisionCheck()
+    private void SpeedLimiter()
     {
-
+        if (_rigidBody.velocity.y < -_maxFallSpeed)
+            _rigidBody.velocity = new Vector2(_rigidBody.velocity.x, -_maxFallSpeed);
     }
 }
